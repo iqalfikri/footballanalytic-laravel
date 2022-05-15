833c8ec73f01f5a662f8355280ca793088f60513
iqalfikri <haiqal@mexious.com>
2022-05-15 10:51:09 +0700

    add blade tamplating football laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<head>
+    @include('includes.meta')
+
+
+    <title>@yield('title') | Landing Page</title>
+
+    <link rel="apple-touch-icon" href="">
+    <link rel="shortcut icon" type="image/x-icon" href="">
+
+    @stack('before-style')
+
+    @include('includes.style')
+
+    @stack('after-style')
+</head>
+
+<body>
+
+    @include('includes.header')
+    @yield('content')
+    @include('includes.footer')
+
+    @stack('before-script')
+
+    @include('includes.script')
+
+    @stack('after-script')
+</body>
+
+</html>
